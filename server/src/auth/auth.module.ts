bf8154686c5fa212422110232ef260b61b65f86b
Trephy <trephyyy@gmail.com>
2023-03-18 11:14:07 +0200

    Initial Commit

---
+++ server/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { UsersService } from 'src/users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { UsersModule } from 'src/users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  controllers: [AuthController],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UsersModule,
+    PassportModule,
+    JwtModule,
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+})
+export class AuthModule {}
